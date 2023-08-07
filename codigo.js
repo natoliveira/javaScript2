645e523fcab66d533429695f434fa695cc533351
Natália Oliveira <63076575+natoliveira@users.noreply.github.com>
2023-08-07 16:27:11 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,12 @@
+function Soma(a,b) {
+    if (a + b === 50) {
+      console.log( (a + b) + " e igual a 50.");
+    } else {
+      console.log( (a + b)  + " nao e igual a 50 .");
+    }
+  }
+
+  console.log(Soma(25,25))
+  
+  
+  
